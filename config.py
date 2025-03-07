d865d616b192db97fa74600da61ad12634e6fe6e
3zzam69 <147944974+3zzam69@users.noreply.github.com>
2025-03-07 17:15:22 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# ملف config.py
-
-API_ID = "27155532"  # ضع هنا API_ID الخاص بك
-API_HASH = "4b49a9b7bc41ad7f3c66d88a47f24ecc"  # ضع API_HASH الخاص بك
-BOT_TOKEN = "7809990459:AAHnk6DHKeox2iyLA9mOKge4d02rW7O67n8"  # ضع التوكن الخاص بالبوت
